b231a95093d84257cc5fadc7ff032873511593ad
johnPaslawski <jpaslawski93@gmail.com>
2021-05-05 21:26:26 +0200

    Key to the Gate added, responsive Gate added, going to next world started

---
+++ Assets/Source/Actors/Static/Gate.cs
@@ -0,0 +1,23 @@
+﻿using DungeonCrawl.Actors.Characters;
+using UnityEngine;
+
+namespace DungeonCrawl.Actors.Static
+{
+    public class Gate : Actor
+    {
+        public override int DefaultSpriteId => 919;
+        public override string DefaultName => "Gate";
+
+        public override bool OnCollision(Actor anotherActor)
+        {
+            bool isKeyPresent;
+            
+            isKeyPresent = anotherActor.IsKeyPresent();
+            Debug.Log($"STATUS OF THE KEY IS : {isKeyPresent}");
+            return isKeyPresent;
+        }
+
+        
+    }
+
+}
